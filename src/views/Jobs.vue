0002c15cc38c606bbce842929134b0882a9cd207
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-24 22:38:06 +0100

    create and update Jobs.vue component

---
+++ src/views/Jobs.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+      <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+jobs: [
+    { title: 'Yushi UX Designer', id: 1, details: 'lorem' },
+    { title: 'Yushi Web Developer', id: 2, details: 'lorem' },
+    { title: 'Yushi Vue Developer', id: 3, details: 'lorem' }
+]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
