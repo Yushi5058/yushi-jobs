5245b9cdefc8e13a35b6d3e5c5bcd01ca15f7a11
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-24 22:26:21 +0100

    update files

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+      <h1>Homepage</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat incidunt cum nesciunt, natus maiores delectus quia dolorem ipsa adipisci accusamus quo quasi quam rerum iure saepe aliquid corrupti vel beatae quos similique doloremque aut odio dolor. Laboriosam optio beatae maiores voluptatibus pariatur expedita explicabo nemo minima corporis mollitia vel, culpa cupiditate ad enim sequi aliquam dignissimos voluptas. Et aperiam eligendi architecto vero eos, facere, provident consectetur dolores nihil unde reprehenderit assumenda labore laborum quis saepe, necessitatibus alias aspernatur dolor. Cum facilis voluptatem blanditiis maxime veniam obcaecati odit. Adipisci ducimus sit unde, eos atque quas quisquam natus soluta distinctio necessitatibus?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia fugiat incidunt cum nesciunt, natus maiores delectus quia dolorem ipsa adipisci accusamus quo quasi quam rerum iure saepe aliquid corrupti vel beatae quos similique doloremque aut odio dolor. Laboriosam optio beatae maiores voluptatibus pariatur expedita explicabo nemo minima corporis mollitia vel, culpa cupiditate ad enim sequi aliquam dignissimos voluptas. Et aperiam eligendi architecto vero eos, facere, provident consectetur dolores nihil unde reprehenderit assumenda labore laborum quis saepe, necessitatibus alias aspernatur dolor. Cum facilis voluptatem blanditiis maxime veniam obcaecati odit. Adipisci ducimus sit unde, eos atque quas quisquam natus soluta distinctio necessitatibus?</p>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {  }
+}
+</script>
